2964e053ed6e215059ad5ad720d92f04852b132c
thierno953 <77080504+thierno953@users.noreply.github.com>
2023-03-06 15:59:20 +0100

    first commit

---
+++ 03-advanced-react/src/App.jsx
@@ -0,0 +1,13 @@
+import ToggleExample from "./tutorial/08-custom-hooks/final/01-toggle";
+import FetchData from "./tutorial/08-custom-hooks/final/02-fetch-data";
+
+
+function App() {
+  return (
+    <div className='container'>
+      <FetchData />
+    </div>
+  );
+}
+
+export default App;
